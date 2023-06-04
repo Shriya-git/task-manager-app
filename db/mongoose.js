040a4a66d3cc8d314f18e2dfd191c87ef5b9d8a1
Shriya Gupta <shriyamanpur@gmail.comm>
2023-06-04 22:30:52 +0530

    Final commit

---
+++ db/mongoose.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.9ll3mzs.mongodb.net/taskMgDB', { useUnifiedTopology: true } )
---
+++ models/task.js
@@ -0,0 +1,16 @@
+const mongoose =require('mongoose')
+
+const Task = mongoose.model('Task',{
+    desc : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    done :{
+        type : Boolean,
+        default: false
+    }
+
+})
+
+module.exports = Task
